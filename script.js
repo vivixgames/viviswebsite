1ccc90ccab8c6458ea5596af6f60ebf3a26d7c4d
vivixgames <vivixadventures@gmail.com>
2025-08-29 21:49:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// script.js
+document.addEventListener("click", function(e) {
+  let sparkle = document.createElement("div");
+  sparkle.className = "sparkle";
+  sparkle.style.left = e.pageX + "px";
+  sparkle.style.top = e.pageY + "px";
+  document.body.appendChild(sparkle);
+
+  setTimeout(() => {
+    sparkle.remove();
+  }, 1000);
+});
 
